bfd865c3cfe35fd84d292e59fb101b9a05db8b8c
Unknown <andrewpetersell@gmail.com>
2017-05-03 00:53:37 -0400

    Game Loop More or Less Working

--- Codelab 1 Final/Assets/Scripts/TokenSpawner.cs
+++ Codelab 1 Final/Assets/Scripts/TokenSpawner.cs
@@ -4,69 +4,32 @@ using UnityEngine;
 
 public class TokenSpawner : MonoBehaviour {
 
-	public GameObject rockToken;
-	public GameObject paperToken;
-	public GameObject scissorsToken; 
-	private Vector2 randomLocation; 
+	public GameObject token;
+	public float timeUntilSpawn;
+	public float maxX;
 	public float minX;
-	public float maxX; 
+	public float maxY;
 	public float minY;
-	public float maxY; 
-	public bool rockGone;
-	public int rockTimer;
-	public bool paperGone; 
-	public int paperTimer;
-	public bool scissorsGone; 
-	public int scissorsTimer; 
-	public int timerLimit;
 
 
 
 	// Use this for initialization
 	void Start () {
-		
+
+		timeUntilSpawn = Random.Range (5, 10);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		randomLocation = new Vector2 (Random.Range (minX, maxX), Random.Range (minY, maxY)); 
-
-		if (rockGone == true) 
+		timeUntilSpawn = timeUntilSpawn - Time.deltaTime;
+		if (timeUntilSpawn <= 0) 
 		{
-			rockTimer++; 
-		}
-		if (paperGone == true) 
-		{
-			paperTimer++;
-		}
-		if (scissorsGone == true) 
-		{
-			scissorsTimer++;
+			token.gameObject.SetActive (true);
+			token.transform.position = new Vector2 (Random.Range (minX, maxX), Random.Range (minY, maxY));
+			timeUntilSpawn = Random.Range (5, 10);
 		}
 
-		if (rockTimer >= timerLimit) 
-		{
-			Instantiate (rockToken, randomLocation, Quaternion.identity);
-			rockGone = false;
-			rockTimer = 0;
-		}
-
-		if (paperTimer >= timerLimit) 
-		{
-			Instantiate (paperToken, randomLocation, Quaternion.identity);
-			paperGone = false;
-			paperTimer = 0;
-		}
-
-
-		if (scissorsTimer >= timerLimit) 
-		{
-			Instantiate (scissorsToken, randomLocation, Quaternion.identity);
-			scissorsGone = false;
-			scissorsTimer = 0;
-		}
-		
 		
 	}
 }
